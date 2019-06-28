5a7cfa0992190bcd7a840242baa627bd55378f01
Sayeeda Tabassum <43369932+sayeedatabassum@users.noreply.github.com>
2019-06-28 19:57:28 +0530

    Update tictactoegame.cpp

--- tictactoegame.cpp
+++ tictactoegame.cpp
@@ -22,11 +22,11 @@ void display_board()
 	cout<<"\t\t_____|_____|_____\n"; 
   
  	cout<<"\t\t     |     |     \n";
-    cout<<"\t\t  "<<board[1][0]<<"  |  "<<board[1][1]<<"  |  "<<board[1][2]<<"  \n";
+    	cout<<"\t\t  "<<board[1][0]<<"  |  "<<board[1][1]<<"  |  "<<board[1][2]<<"  \n";
 	cout<<"\t\t_____|_____|_____\n"; 
   
   	cout<<"\t\t     |     |     \n";
-    cout<<"\t\t  "<<board[2][0]<<"  |  "<<board[2][1]<<"  |  "<<board[2][2]<<"  \n";
+   	cout<<"\t\t  "<<board[2][0]<<"  |  "<<board[2][1]<<"  |  "<<board[2][2]<<"  \n";
 	cout<<"\t\t     |     |     \n"; 
 }
 
